ece90600093a9aff51f8db36b353ca8e532ba51e
fuzhiquan <fzqapollo@163.com>
2022-04-10 18:05:32 +0800

    feat: 项目初始化

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const VSHADER_SOURCE = `
+void main() {
+    gl_Position = vec4(0.5,0.0,0.0,1.0);
+    gl_PointSize = 10.0;
+}`
+const FSHADER_SOURCE = `
+void main() {
+    gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+}`
+
+function main() {
+    const canvas = document.getElementById('webgl')
+    const gl = getWebGLContext(canvas)
+
+    const initState = initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)
+
+    gl.clearColor(0.3,0.3,0.3,1.0)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    gl.drawArrays(gl.POINTS, 0, 1)
+}
+
+main()
